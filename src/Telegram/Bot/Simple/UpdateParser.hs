26a77a85d9ffba7ee8820dcb0240913db7bf21b5
Anatolii Prylutskyi <anpryl@gmail.com>
2019-03-30 15:30:10 +0200

    Rework message parser

--- src/Telegram/Bot/Simple/UpdateParser.hs
+++ src/Telegram/Bot/Simple/UpdateParser.hs
@@ -47,10 +47,9 @@ plainText = do
 message :: Text -> UpdateParser Text
 message msg = do
   t <- text
-  case Text.words t of
-    (w:ws) | w == msg
-      -> pure (Text.unwords ws)
-    _ -> fail "not that command"
+  if msg `Text.isPrefixOf` t
+  then pure $ Text.drop (Text.length msg + 1) t
+  else fail "not that message"
 
 command :: Text -> UpdateParser Text
 command name = do
