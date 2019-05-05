cca44b40c3c3f95f8292e44df6010f2859e94caa
Anatolii Prylutskyi <anpryl@gmail.com>
2019-05-05 11:32:06 +0300

    Extend Delete module with helpers

--- src/Telegram/Bot/Simple/Delete.hs
+++ src/Telegram/Bot/Simple/Delete.hs
@@ -2,16 +2,29 @@ module Telegram.Bot.Simple.Delete where
 
 import           Control.Monad.Reader
 
-import           Telegram.Bot.API (MessageId)
+import           Telegram.Bot.API (ChatId, MessageId, messageMessageId)
 import           Telegram.Bot.Simple.Eff
 import           Telegram.Bot.Simple.Reply
 
 import qualified Telegram.Bot.API     as Telegram
+
+-- | Delete message from chat
+deleteChatMessage :: ChatId -> MessageId -> BotM ()
+deleteChatMessage chatId = void . liftClientM . Telegram.deleteMessage chatId
 
 -- | Delete message in the current chat (if possible).
 deleteMessage :: MessageId -> BotM ()
 deleteMessage msgId = do
   mchatId <- currentChatId
   case mchatId of
-    Just chatId -> void $ liftClientM $ Telegram.deleteMessage chatId msgId
+    Just chatId -> deleteChatMessage chatId msgId
     Nothing     -> liftIO $ putStrLn "No chat to delete message"
+
+-- | Delete current update message from current chat (if possible).
+deleteUpdateMessage :: BotM ()
+deleteUpdateMessage = do
+  mupdate <- asks botContextUpdate
+  let mmsgId = fmap messageMessageId . Telegram.extractUpdateMessage =<< mupdate
+  case mmsgId of
+    Just msgId -> deleteMessage msgId
+    Nothing -> liftIO $ putStrLn "No message to delete"
