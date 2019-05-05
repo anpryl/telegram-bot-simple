c5592a19182c57eb131cc4e9b0f051797b36d036
Anatolii Prylutskyi <anpryl@gmail.com>
2019-05-05 10:13:27 +0300

    Update to latest nixpkgs with ghc 8.4.3

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let importNixPkgs = rev:
       import (fetchNixPkgs rev) {};
     fetchNixPkgs = rev:
       builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    pkgs = importNixPkgs "069bf7aee30faf7b3ed773cfae2154d761b2d6c2";
+    pkgs = importNixPkgs "55a82ffcd3090086a935d127c6fb5cd7eabe1f8a";
 in pkgs.haskell.lib.buildStackProject {
   name = "telegram-bot-simple";
   buildInputs = with pkgs; [ gmp zlib ];
