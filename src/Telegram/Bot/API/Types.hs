8bf05d2055b80160ce5ab09d3f30e5a1aa737406
Anatolii Prylutskyi <anpryl@gmail.com>
2019-09-15 22:30:26 +0300

    Put message id to int32 back

--- src/Telegram/Bot/API/Types.hs
+++ src/Telegram/Bot/API/Types.hs
@@ -136,10 +136,10 @@ data Message = Message
   } deriving (Generic, Show)
 
 -- | Unique message identifier inside this chat.
-newtype MessageId = MessageId Integer
+newtype MessageId = MessageId Int32
   deriving (Eq, Show, ToJSON, FromJSON)
 
-instance ToHttpApiData MessageId where toUrlPiece a = pack . show @Integer $ coerce a
+instance ToHttpApiData MessageId where toUrlPiece a = pack . show @Int32 $ coerce a
 
 -- | The unique identifier of a media message group a message belongs to.
 newtype MediaGroupId = MediaGroupId Text
