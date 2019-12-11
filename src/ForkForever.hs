58143d5a7dfa26878114521ed3d67e7367e639ce
Anatolii Prylutskyi <anpryl@gmail.com>
2019-12-12 00:24:17 +0200

    Update ForkForever to handle async exceptions

--- src/ForkForever.hs
+++ src/ForkForever.hs
@@ -1,14 +1,13 @@
 module ForkForever where
 
-import           Control.Concurrent     (ThreadId, forkIO)
-import           Control.Exception.Safe (catchAny)
-import           Control.Monad          (void)
-import           Data.Function          (fix)
+import           Control.Concurrent       (ThreadId)
+import           Control.Concurrent.Async (waitCatch, withAsync)
+import           Data.Function            (fix)
 
 forkForever_ :: IO a -> IO ()
 forkForever_ act = forkForever act >> pure ()
 
 forkForever :: IO a -> IO ThreadId
-forkForever act = forkIO $ fix $ \next -> do
-    void act `catchAny` (print . ("Thread died: " <>) . show)
-    next
+forkForever act = fix $ \next -> do
+  withAsync act $ \a -> waitCatch a >>= either (print . ("Thread died: " <>) . show) (const $ pure ())
+  next
